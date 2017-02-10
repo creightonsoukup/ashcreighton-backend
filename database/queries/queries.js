0b4a19f07d5108002a3a4406d9419582d51671aa
Your Name <creighton@creightonsoukup.com>
2017-02-09 18:50:04 -0700

    added new queries and routes

--- database/queries/queries.js
+++ database/queries/queries.js
@@ -1,10 +1,60 @@
 var knex = require('./knex.js');
 
-function getVc () {
+function getAllVc () {
   return knex('vc').orderBy('name', 'asce')
+}
+
+function editVc (id, name, type, city, state, country, website, description, date_added, investments_last_twelve, active_portfolio, last_fund_size, last_fund_date, dry_powder, exits, year_founded, investment_range_low, investment_range_high, investments) {
+  return getAllVc().where('id', id).update({
+    name : name,
+    type: type,
+    city: city,
+    state: state,
+    country: country,
+    website: website,
+    description: description,
+    date_added: date_added,
+    investments_last_twelve: investments_last_twelve,
+    active_portfolio: active_portfolio,
+    last_fund_size: last_fund_size,
+    last_fund_date: last_fund_date,
+    dry_powder: dry_powder,
+    exits: exits,
+    year_founded: year_founded,
+    investment_range_low: investment_range_low,
+    investment_range_high: investment_range_high,
+    investments: investments,
+    updated: new Date()
+  })
+}
+
+function newVc (id, name, type, city, state, country, website, description, date_added, investments_last_twelve, active_portfolio, last_fund_size, last_fund_date, dry_powder, exits, year_founded, investment_range_low, investment_range_high, investments) {
+  return getAllVc().insert({
+    name : name,
+    type: type,
+    city: city,
+    state: state,
+    country: country,
+    website: website,
+    description: description,
+    date_added: date_added,
+    investments_last_twelve: investments_last_twelve,
+    active_portfolio: active_portfolio,
+    last_fund_size: last_fund_size,
+    last_fund_date: last_fund_date,
+    dry_powder: dry_powder,
+    exits: exits,
+    year_founded: year_founded,
+    investment_range_low: investment_range_low,
+    investment_range_high: investment_range_high,
+    investments: investments,
+    updated: new Date()
+  })
 }
 
 
 module.exports = {
-  getVc
+  getAllVc,
+  newVc,
+  editVc
 }
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,40 @@ var queries = require('../database/queries/queries.js')
 
 /* GET home page. */
 router.get('/vc', function(req, res, next) {
-    queries.getVc()
+    queries.getAllVc()
       .then((data) => {
         res.send(data)
       })
 });
+
+router.post('/vc/update', function(req, res, next) {
+  queries.editVc(req.body.id, req.body.name,
+    req.body.type, req.body.city, req.body.state,
+    req.body.country, req.body.webiste, req.body.description,
+    req.body.date_added, req.body.investments_last_twelve,
+    req.body.active_portfolio, req.body.last_fund_size,
+    req.body.last_fund_date, req.body.dry_powder,
+    req.body.exits, req.body.year_founded,
+    rec.body.investment_range_low, rec.body.investment_range_high,
+    req.body.investments)
+      .then((data) => {
+        res.send('success')
+      })
+})
+
+router.post('/vc/new', function(req, res, next) {
+  queries.newVc(req.body.id, req.body.name,
+    req.body.type, req.body.city, req.body.state,
+    req.body.country, req.body.webiste, req.body.description,
+    req.body.date_added, req.body.investments_last_twelve,
+    req.body.active_portfolio, req.body.last_fund_size,
+    req.body.last_fund_date, req.body.dry_powder,
+    req.body.exits, req.body.year_founded,
+    rec.body.investment_range_low, rec.body.investment_range_high,
+    req.body.investments)
+      .then((data) => {
+        res.send('success')
+      })
+})
 
 module.exports = router;
